e58862931a76424df98febd021b898d22b7a3cf0
Leonardo Ferrer <ext.leonardo.ferrer@farfetch.com>
2022-05-29 19:28:11 +0100

    starting form

--- Shufflet/src/screens/Main.js
+++ Shufflet/src/screens/Main.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { StyleSheet, TextInput, View  } from "react-native";
 
 export default function Main () {
-
+  let numberOfLines =  [1,2,3,4,5];
   return (
     <View style = {[style.container]}>
-      <View style = {[style.form]}>
-        <TextInput maxLength={1}  style={[style.input]}/>
-        
-        <TextInput maxLength={1} style={[style.input]}/>
-
-        <TextInput maxLength={1} style={[style.input]} />
-
-        <TextInput maxLength={1} style={[style.input]}  />
-
-        <TextInput maxLength={1} style={[style.input]} />
-      </View>
+      {
+        numberOfLines.map((a) => (
+          getForms(a)
+        ))
+      }
     </View>
   );
 }
@@ -24,6 +18,23 @@ function handleSubmit(){
   console.log(submit)
 }
 
+function getForms(key){
+  return(
+    <View key={key} style = {[style.form]}>
+
+      <TextInput key={key + '1'} maxLength={1}  style={[style.input]}/>
+      
+      <TextInput key={key + '2'} maxLength={1} style={[style.input]}/>
+
+      <TextInput key={key + '3'} maxLength={1} style={[style.input]} />
+
+      <TextInput key={key + '4'} maxLength={1} style={[style.input]}  />
+
+      <TextInput key={key + '5'} maxLength={1} style={[style.input]} />
+
+    </View>
+  )
+}
 
 const style = StyleSheet.create({
   container: {
@@ -36,12 +47,11 @@ const style = StyleSheet.create({
   },
   input: {
     padding:10,
-    margin:10,
+    marginRight:5,
+    marginLeft:5,
     borderWidth: '1px',
     borderColor: 'gray',
     borderRadius: '10%',
-    width: 50,
-    height: 50,
   },
   button: {
     backgroundColor: '#228CDB'
