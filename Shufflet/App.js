b563fba20835ba3405f652b7606b4afa63681b59
Leonardo Ferrer <ext.leonardo.ferrer@farfetch.com>
2022-06-15 10:33:14 +0100

    initial async storage tests

--- Shufflet/App.js
+++ Shufflet/App.js
@@ -5,27 +5,32 @@ import { StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Main from './src/screens/Main';
-import AsyncStorage from '@react-native-community/async-storage';
-import * as RNFS from 'react-native-fs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as FileSystem from 'expo-file-system'
+
 const Stack = createStackNavigator();
 
 export default function App() {
-  AsyncStorage.getItem('fiveChars').then(res => {
-    console.log(res)
-    if(res != undefined){
-
-      RNFS.readFileAssets("../assets/palavras5.txt") // 'base64' for binary 
-        .then(res => {
-          console.log(res)
-          AsyncStorage.setItem('fiveChars', res);
-        })
-        .catch(console.error)
-      AsyncStorage.setItem('fiveChars', 'value');
-      AsyncStorage.setItem('sixChars', 'value');
-      AsyncStorage.setItem('sevenChars', 'value');
-      AsyncStorage.setItem('eightChars', 'value');
+  const storeData = async (value) => {
+    try {
+      AsyncStorage.setItem('fiveCharWord', 'carol');
+      await AsyncStorage.getItem('fiveCharWordx').then(res => {
+        if(res != undefined){
+          FileSystem.readAsStringAsync("/assets/palavras5.txt") // 'base64' for binary 
+            .then(res => {
+              AsyncStorage.setItem('fiveCharWord',res[Math.floor(Math.random() * res.length)]);
+            })
+            .catch(console.error)
+          AsyncStorage.setItem('sixChars', 'value');
+          AsyncStorage.setItem('sevenChars', 'value');
+          AsyncStorage.setItem('eightChars', 'value');
+        }
+      })
+    }catch (e) {
+      // saving error
     }
-  });
+  }
+  storeData()
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerStyle: {backgroundColor: '#228CDB'},headerTintColor: '#fff'}} initialRouteName="Main">
@@ -48,7 +53,6 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
 const style = StyleSheet.create({
   headerIcon: {
     marginRight: 10
--- Shufflet/src/screens/Main.js
+++ Shufflet/src/screens/Main.js
@@ -1,27 +1,96 @@
-import React, { useState, useEffect, createRef  } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, TextInput, View, Text  } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 let numberOfLetters = 5;
-let numberOfLines = [];
-let viewRefs = [];
-let lineRef = [[]];
 
 const Main = ()  => {
-  const [lineFocus, setLineFocus] = useState(['auto','none','none','none','none','none','none'])
+  const [lineFocus, setLineFocus] = useState(['auto','none','none','none','none','none'])
+  const [word, setWord] = useState('')
+  const [coutingChars, setCoutingChars] = useState({})
+  const [viewRefs, setViewRefs] = useState([])
+  const [lineRef, setLineRef] = useState([[]])
+  const [numberOfLines, setNumberOfLines] = useState([])
   numberOfLetters = 5;
-  numberOfLines = [];
-  viewRefs = [];
-  lineRef = [[]];
   useEffect(() => {
-      console.log(lineFocus);
+    configure();
+    AsyncStorage.getItem('fiveCharWord').then((res) => {
+      setWord(res)
+      let newCoutingChars = {}
+      for(let char in res){
+        newCoutingChars[res[char]] = (newCoutingChars[res[char]] || 0) + 1;
+      }
+      setCoutingChars(newCoutingChars)
+    })
   }, []);
   const handleSubmitEditing = (key) => {
-    setLineFocus(['none','auto','none','none','none','none','none'])
-    lineRef[key+1][0].current.focus();
+    if(key < 5){
+      var isSolved = true;
+      var containingChars = [];
+      let coutingCharsAux = coutingChars
+      for(let a = 0;a<viewRefs[key].current.children.length;a++){
+        var char = lineRef[key][a].current.value
+        if(char == ''){
+          console.log('incompleto')
+          lineRef[key][0].current.focus();
+          return;
+        }
+      }
+      for(let a = 0;a<viewRefs[key].current.children.length;a++){
+        var char = lineRef[key][a].current.value
+        if(char == word[a]){
+          lineRef[key][a].current.setNativeProps({
+            style:{backgroundColor: '#3fba29'}
+          });
+          if(coutingCharsAux[char] > 0){ 
+            coutingCharsAux[char]--;
+          }
+        }else if(word.indexOf(char) >= 0){
+          isSolved = false;
+          var containChar = {char:char,a:a};
+          containingChars.push(containChar)
+        }else{
+          isSolved = false;
+        }
+      }
+      if(isSolved){
+        setLineFocus(['none','none','none','none','none','none'])
+        return
+      }
+      for(let b in containingChars){
+        console.log(b.char)
+        if(coutingCharsAux[containingChars[b].char] > 0){
+          lineRef[key][containingChars[b].a].current.setNativeProps({
+            style:{backgroundColor: '#fff700'}
+          });
+          coutingCharsAux[containingChars[b].char]--
+        }
+      }
+      const newLineFocus = (['none','none','none','none','none','none']);
+      newLineFocus[key+1] = 'auto'
+      setLineFocus(newLineFocus)
+      lineRef[key+1][0].current.focus();
+    }
   }
-  configure();
-  return (
+  const configure = () => {
+    for(let a = 0;a < numberOfLetters+1;a++){
+      setNumberOfLines(lines => [...lines,a])
+      setViewRefs(refs => [...refs,React.createRef()])
+      viewRefs.push(React.createRef())
+      for(let b = 0; b<numberOfLetters ; b++){
+        if(b == 0){
+          lineRef[a] = []
+        }
+        setLineRef(refs => {
+          const newRefs = refs;
+          newRefs[a].push(React.createRef())
+          return { ...newRefs }
+        })
+      }
+    }
+  }
+    return (
       <View style = {[style.container]}>
         <Text style = {[style.title]}> Shufflet </Text>
         {
@@ -38,7 +107,8 @@ const Main = ()  => {
                       lineRef[key][1].current.focus();
                     }
                   }}
-                  returnKeyType="next"/>
+                  returnKeyType="next"
+                  onSubmitEditing={() => handleSubmitEditing(key)}/>
                 <TextInput  tabIndex={key + '2'} key={key + '2'} maxLength={1} style={[style.input]}
                   ref={lineRef[key][1]}
                   onChangeText={(value) => {
@@ -46,7 +116,8 @@ const Main = ()  => {
                       lineRef[key][2].current.focus();
                     }
                   }}
-                  returnKeyType="next"/>
+                  returnKeyType="next"
+                  onSubmitEditing={() => handleSubmitEditing(key)}/>
         
                 <TextInput  tabIndex={key + '3'} key={key + '3'} maxLength={1} style={[style.input]}
                   ref={lineRef[key][2]}
@@ -55,7 +126,8 @@ const Main = ()  => {
                       lineRef[key][3].current.focus();
                     }
                   }}
-                  returnKeyType="next"/>
+                  returnKeyType="next"
+                  onSubmitEditing={() => handleSubmitEditing(key)}/>
         
                 <TextInput  tabIndex={key + '4'} key={key + '4'} maxLength={1} style={[style.input]}
                   ref={lineRef[key][3]}
@@ -64,31 +136,19 @@ const Main = ()  => {
                       lineRef[key][4].current.focus();
                     }
                   }}
-                  returnKeyType="next"/>
+                  returnKeyType="next"
+                  onSubmitEditing={() => handleSubmitEditing(key)}/>
         
                 <TextInput  tabIndex={key + '5'} key={key + '5'} maxLength={1} style={[style.input]}
                   ref={lineRef[key][4]}
-                  returnKeyType="next"/>
+                  returnKeyType="next"
+                  onSubmitEditing={() => handleSubmitEditing(key)}/>
             </View>
           ))
         }
       </View>
         
   );
-}
-
-function configure(){
-  for(let a = 0;a < numberOfLetters+1;a++){
-    numberOfLines.push(a)
-    viewRefs.push(React.createRef())
-    for(let b = 0; b<numberOfLetters ; b++){
-      if(b == 0){
-        lineRef[a] = []
-      }
-      lineRef[a].push(React.createRef());
-    }
-  }
-  console.log(numberOfLines)
 }
 
 
